7f5d5ea2ec89de68c48bd2b0f64cc49c38bd089c
amit <amit1.mnnit@gmail.com>
2015-05-26 16:22:32 +0530

    temp ui state

--- simple-todos.js
+++ simple-todos.js
@@ -3,17 +3,31 @@ Tasks = new Mongo.Collection("tasks");
 if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
-    tasks: function () {
-      return Tasks.find({}, {sort: {createdAt: -1}});
-    }
-  });
+  tasks: function () {
+    if (Session.get("hideCompleted")) {
+      // If hide completed is checked, filter tasks
+      return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+    } else {
+      	// Otherwise, return all of the tasks
+      	return Tasks.find({}, {sort: {createdAt: -1}});
+    	}
+  	},
+  	hideCompleted: function () {
+    	return Session.get("hideCompleted");
+  	},
+  	incompleteCount: function () {
+  		return Tasks.find({checked: {$ne: true}}).count();
+	}
+	});
 
   Template.body.events({
+  	"change .hide-completed input": function (event) {
+  	Session.set("hideCompleted", event.target.checked);
+	},
+
   	"submit .new-task": function (event) {
     // This function is called when the new task form is submitted
-
     var text = event.target.text.value;
-
     Tasks.insert({
       text: text,
       createdAt: new Date() // current time
